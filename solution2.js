814a4241223eec4781afae6e7ba83e3895ace160
sovonbd <sovonsdp@gmail.com>
2024-02-27 12:27:18 -0600

    updated

--- solution2.js
+++ solution2.js
@@ -37,20 +37,6 @@ app.get("/", async (req, res) => {
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
 
-  // const result = await db.query(
-  //   "SELECT country_code FROM countries WHERE country_name = $1",
-  //   [input]
-  // );
-
-  // if (result.rows.length !== 0) {
-  //   const data = result.rows[0];
-  //   const countryCode = data.country_code;
-
-  //   await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
-  //     countryCode,
-  //   ]);
-  //   res.redirect("/");
-  // }
   const result = await db.query(
     "SELECT country_code FROM countries WHERE country_name = $1",
     [input]
